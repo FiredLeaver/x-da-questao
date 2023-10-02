144912540c09737b9211bc8613164467e6c416f1
Julio <julioantunesestrela13@gmail.com>
2023-10-01 21:14:33 -0300

    Add: account popup at index page

--- frontend/index/index.js
+++ frontend/index/index.js
@@ -1,16 +1,35 @@
 function toogleNav() {
     if (document.getElementById("mySidenav").style.width == "250px") {
-        closeNav();
-    }
-    else{
-        openNav();
-    }
+      closeNav();
   }
+    else{
+      openNav();
+  }
+}
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
+  document.getElementById("mySidenav").style.width = "250px";
+}
   
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+function toogleAccountPopUp(){
+  console.log("yeey")
+  if (document.getElementById("account-popup").style.display == "flex") {
+    closeAccountPopUp();
   }
+  else{
+    openAccountPopUp();
+  }
+}
+
+function openAccountPopUp(){
+  document.getElementById("account-popup").style.display = "flex";
+}
+
+function closeAccountPopUp(){
+  document.getElementById("account-popup").style.display = "none";
+}
